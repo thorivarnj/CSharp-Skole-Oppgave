662e43d25cf0ea999b0661cf9369c81412d33f3d
Thor Ivar Nirisen Johansen <thor5674@gmail.com>
2019-12-10 22:11:23 +0100

    first commit

---
+++ DrNimGame/DrNimGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrNimGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   ---   Dr Nim Game   ---   ");
+        }
+    }
+}
